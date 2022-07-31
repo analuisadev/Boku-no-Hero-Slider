68266c99c00cab3fd30129a4f2215a78bc35c435
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2022-07-30 22:08:58 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+const btnNext = document.getElementById('btn__next');
+const btnBack = document.getElementById('btn__back');
+const card = document.querySelectorAll('.pokemonList__card');
+let ActualCard = 0;
+
+
+function HideSelectedCard () {
+    const SelectedCard = document.querySelector('.card__select');
+    SelectedCard.classList.remove('card__select');
+}
+
+function showCard(indiceCard) {
+    card[indiceCard].classList.add('card__select');
+}
+
+btnNext.addEventListener('click', function () {
+
+    if(ActualCard === card.length -1) {
+        return;
+    }
+
+    HideSelectedCard();
+    ActualCard ++ ;
+    showCard(ActualCard);
+});
+
+btnBack.addEventListener('click', function () {
+
+    if (ActualCard === 0) {
+        return;
+    }
+
+    HideSelectedCard();
+
+    
+    ActualCard -- ;
+    showCard(ActualCard);
+});
